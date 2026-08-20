c3c4ea03e1a53aa8c30b77dca88cae154fa4d3be
Tales Aragão <talessba@gmail.com>
2026-08-20 20:10:08 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,7 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner sc = new Scanner(System.in);
 		
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
@@ -46,6 +47,15 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("Update complete");
 		
+		System.out.println("\n=== TEST 6: seller delete ====");
+		System.out.println("Enter id fot delete test: ");
+		int id = sc.nextInt();
+		sellerDao.findById(id);
+		System.out.println("DELETE COMPLETED! ");
+
+		sc.close();
+		
+
 
 		
 
